e76a48d38c93c44052366bbe64509d6ff140de39
Steve Chen <stevechen.job@gmail.com>
2022-01-08 13:07:28 -0500

    upgrade app version

--- lib/models/app_const.dart
+++ lib/models/app_const.dart
@@ -80,7 +80,7 @@ class AppConst{
   static String get submitMobileLog => baseUrl+"submit-mobile-log";
 
   static const String LOG_FILE_PREFIX = "app_log_";
-  static const int currentVersion = 46;
+  static const int currentVersion = 47;
 }
 
 class GlobalData{
